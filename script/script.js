bb412f58d57ec386b9387241721bb1d9a1cd42c3
ITF-19-B-21 <itf-19-b-21@no.de>
2021-03-24 10:56:24 +0100

    Finally, working login, logout and register. Again

--- script/script.js
+++ script/script.js
@@ -35,39 +35,3 @@ $(document).ready(function(){
     }
     
     ajaxRequest();
-
-
-
-
-    function mainPage() {
-      //create elements
-      let bootdeyDiv = document.createElement("div");
-      let mainNavDiv  = document.createElement("div");
-      let searchSection = document.createElement("section");
-      let searchDiv = document.createElement("div");
-      let searchInput = document.createElement("input");
-      let categorySection = document.createElement("section");
-      let categoryHeader = document.createElement("header");
-      let categoryDiv = document.createElement("div");
-      let categoryUl = document.createElement("ul");
-
-      let categoryLi1 = document.createElement("li");
-      let categoryA1 = document.createElement("a");
-      let categoryI1 = document.createElement("i");
-
-      let categoryLi2 = document.createElement("li");
-      let categoryA2 = document.createElement("a");
-      let categoryI2 = document.createElement("i");
-
-      let categoryLi3 = document.createElement("li");
-      let categoryA3 = document.createElement("a");
-      let categoryI3 = document.createElement("i");
-
-      let categoryLi4 = document.createElement("li");
-      let categoryA4 = document.createElement("a");
-      let categoryI4 = document.createElement("i");
-     
-
-
-      categorySection.appendChild(categoryHeader);
-    }
