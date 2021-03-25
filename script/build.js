270889154659f0a0556c940eb4ef18206961c290
JonasHe46 <jomihei@gmx.de>
2021-03-25 22:21:12 +0100

    [add] Elemente werden bei Klick auf die Kategorie angezeigt :)

--- script/build.js
+++ script/build.js
@@ -17,7 +17,7 @@
 
         //call elementsß
         addSearch(mainNavDiv);
-        addCategory(data, mainNavDiv);
+        addCategory(data, mainNavDiv, bootdeyDiv);
         addProducts(data, mainNavDiv);
 
   
@@ -43,7 +43,7 @@
         mainNavDiv.appendChild(searchSection);
       }
 
-      function addCategory(data, mainNavDiv) {
+      function addCategory(data, mainNavDiv, bootdeyDiv) {
        //create elements
        let categorySection = document.createElement("section");
        let categoryHeader = document.createElement("header");
@@ -77,7 +77,7 @@
 
         //  categoryA.onclick = category;
         categoryA.addEventListener('click', function(){
-          category(data, id);
+          category(data, id, bootdeyDiv);
       });
          //append
          categoryA.appendChild(categoryI);
@@ -117,18 +117,69 @@
         console.log("function switch category")
       }
 
-      function category(data, id) {
+      function category(data, id, bootdeyDiv) {
+        let cartMainDiv = document.createElement("div");
+        let cartPanelSelection = document.createElement("section");
+
+        let cartDiv = document.createElement("div");
+
+        cartPanelSelection.classList.add("panel");
+        cartMainDiv.classList.add("col-md-9");
+
+        cartDiv.classList.add("row");
+        cartDiv.classList.add("product-list");
+
+
         // console.log("data: ", data, " id: ", id)
         console.log("click")
         Object.keys(data["Artikel"]["A_IDS"]).forEach(a_IDS => {
           if(id.substring(8) == data["Artikel"]["A_KategorieID"][a_IDS]) {
             // console.log("this id: ", id.substring(8))
             // console.log("A_IDS: ", a_IDS)
-            console.log(data["Artikel"]["A_Name"][a_IDS])
-        
+            // console.log(data["Artikel"]["A_Name"][a_IDS])
+            let rowDiv = document.createElement("div");
+            rowDiv.classList.add("col-md-4");
+            let productName = data["Artikel"]["A_Name"][a_IDS]
+            let section = document.createElement("section");
+            section.classList.add("panel");
+            let div = document.createElement("div");
+            div.classList.add("pro-img-box");
+            let img = document.createElement("img");
+            img.setAttribute("alt", productName);
+            img.setAttribute("src", "img/" + productName + ".png")
+            let a = document.createElement("a");
+            a.classList.add("adtocart");
+            a.setAttribute("href", "#");
+             let i = document.createElement("i");
+             i.classList.add("fa");
+             i.classList.add("fa-shopping-cart");
+             let div2 = document.createElement("div");
+             div2.classList.add("panel-body");
+             div2.classList.add("text-center");
+             let h4 = document.createElement("h4");
+             let a2 = document.createElement("a");
+             a2.classList.add("pro-title");
+             a2.innerHTML = productName;
+             let p= document.createElement("a");
+             p.classList.add("price");
+             p.innerHTML = "$" + data["Artikel"]["A_Preis"][a_IDS];
+
+             div.appendChild(img);
+             a.appendChild(i);
+             div.appendChild(a);
+             section.appendChild(div);
+             h4.appendChild(a2);
+             div2.appendChild(h4);
+             div2.appendChild(p);
+             section.appendChild(div2);
+             rowDiv.appendChild(section);
+             cartDiv.appendChild(rowDiv);
           }
         })
 
-       
+
+        cartMainDiv.appendChild(cartPanelSelection);
+        cartMainDiv.appendChild(cartDiv);
+        bootdeyDiv.appendChild(cartMainDiv);
       }
       export { mainPage };
